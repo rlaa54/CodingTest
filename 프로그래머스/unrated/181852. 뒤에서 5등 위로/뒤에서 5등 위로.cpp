8f3173cbed6ac5d0f1a0bc61b195e25aa60bf127
김민웅 <32154232+rlaa54@users.noreply.github.com>
2023-10-10 10:41:19 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    sort(num_list.begin(), num_list.end());
+    answer = {num_list.begin() + 5, num_list.end()};
+    return answer;
+}
